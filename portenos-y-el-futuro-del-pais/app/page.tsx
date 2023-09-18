3cede535be55f42f0e3ccb1ab86727f8e4ae6e94
Luz Alba Posse <66437312+luzalbaposse@users.noreply.github.com>
2023-09-18 18:18:33 -0300

    Update page.tsx
    bg-white

--- portenos-y-el-futuro-del-pais/app/page.tsx
+++ portenos-y-el-futuro-del-pais/app/page.tsx
@@ -3,7 +3,7 @@ import NavigationButtons from '../app/components/NavButtons'
 export default function Home() {
 
   return (
-    <main className="flex min-h-screen max-h-screen flex-col items-center justify-center p-24 bg-cover bg-center" style={{ backgroundImage: `url(bgimage.png)` }}>
+    <main className="flex min-h-screen max-h-screen flex-col items-center justify-center p-24 bg-cover bg-center bg-white" style={{ backgroundImage: `url(bgimage.png)` }}>
   <div className="flex justify-between w-15 py-3 px-6 fixed top-2 left-0 right-0">
       <div className="text-black font-montserrat text-lg font-normal mb-2 px-6">Visualización de Datos</div>
       <div className="text-black font-montserrat text-lg font-normal mb-2">S2</div>
